e58c01c49c5b41f8e5c7c31a0a12022ba25b8080
davisdespacito <ddpessetto10@gmail.com>
2026-02-20 21:21:48 -0700

    finished implementing sumOfDigits, all jUnit tests passed; yet to implement ToUpper. 9:21pm

--- CS1410_RecursionJUnit/src/recursion/Recursion.java
+++ CS1410_RecursionJUnit/src/recursion/Recursion.java
@@ -6,13 +6,31 @@ public class Recursion {
 	 * takes input <code>n</code> and sums the digits.
 	 * 
 	 * @param n
-	 * @return
+	 * @return sum of all the digits provided.
 	 */
 	public static int sumOfDigits(int n) {
 
 		// TODO
+		if (n == 0) {
 
-		return 0;
+			return 0;
+
+		}
+
+		if (String.valueOf(n).length() <= 1) {
+
+			return n;
+
+		}
+
+		if (String.valueOf(n).charAt(0) == '-') {
+
+			return sumOfDigits(Integer.parseInt(String.valueOf(n).substring(1)));
+
+		}
+
+		return Integer.parseInt(String.valueOf(n).substring(0, 1))
+				+ sumOfDigits(Integer.parseInt(String.valueOf(n).substring(1)));
 
 	}
 
--- CS1410_RecursionJUnit/test/recursion/SumOfDigitsTest.java
+++ CS1410_RecursionJUnit/test/recursion/SumOfDigitsTest.java
@@ -21,11 +21,15 @@ class SumOfDigitsTest {
 	@Test
 	void sumOfDigits_positiveDigit() {
 		assertEquals(1, Recursion.sumOfDigits(1));
+		System.err.printf("%d is expected; actual is %d %n", 0,
+				Recursion.sumOfDigits(1));
 	}
 
 	@Test
 	void sumOfDigits_negativeDigit() {
 		assertEquals(5, Recursion.sumOfDigits(-5));
+		System.err.printf("%d is expected; actual is %d %n", 0,
+				Recursion.sumOfDigits(5));
 	}
 
 	@Test
