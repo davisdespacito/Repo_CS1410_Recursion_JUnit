07d3b8dd5ce7fc5fbb72ada9716669357a729f31
davisdespacito <ddpessetto10@gmail.com>
2026-02-20 20:35:14 -0700

    added test files and jUnit 6 to build path; 8:35pm

--- CS1410_RecursionJUnit/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module CS1410_RecursionJUnit {
-}
---
+++ CS1410_RecursionJUnit/src/recursion/Recursion.java
@@ -0,0 +1,32 @@
+package recursion;
+
+public class Recursion {
+
+	/**
+	 * takes input <code>n</code> and sums the digits.
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public static int sumOfDigits(int n) {
+
+		// TODO
+
+		return 0;
+
+	}
+
+	/**
+	 * returns input string <code>str</code> as uppercase.
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static String toUpper(String str) {
+
+		// TODO
+		return null;
+
+	}
+
+}
